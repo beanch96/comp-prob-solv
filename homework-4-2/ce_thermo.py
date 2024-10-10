42c19c813bfd47f2020d07f9e9af50c388962e0b
Carlos Bean <b.carlos@wustl.edu>
2024-10-10 12:09:58 -0500

    updates on ce_thermo.py

--- homework-4-2/ce_thermo.py
+++ homework-4-2/ce_thermo.py
@@ -72,23 +72,23 @@ U_NO_SOC = -np.gradient(np.log(Z_Ce3plus_NO_SOC), 1 / (k_B * T))  #internal ener
 F_NO_SOC=-k_B*T*np.log(Z_Ce3plus_NO_SOC)  #Free-energy
 S_NO_SOC= -np.gradient(F_NO_SOC,T)   #Entropy
 
-#plot
-plt.figure(figsize=(6, 4))
-plt.plot(T, U_NO_SOC, color='green',label='Internal Energy U')
-plt.plot(T,F_NO_SOC,color="blue",label='Free Energy F')
-plt.plot(T,S_NO_SOC,"--",color="brown",label='Entropy S')
-plt.xlabel('Temperature (K)')
-plt.ylabel('Internal Energy (eV)')
-plt.title('Energies for Isolated Ce$^{3+}$ vs Temperature')
-plt.legend()
-#Save plot to "homework-4-2"
-directory = 'homework-4-2'
-if not os.path.exists(directory):            #Create Directory
-    os.makedirs(directory)
-plot_filename = os.path.join(directory, "E vs T (isolated Ce3+).png")  #Title of file
-plt.savefig(plot_filename)
-#indicate succesful operation 
-print("Isolated Ce3+ Plot saved to Directory:", directory)
+# #plot
+# plt.figure(figsize=(6, 4))
+# plt.plot(T, U_NO_SOC, color='green',label='Internal Energy U')
+# plt.plot(T,F_NO_SOC,color="blue",label='Free Energy F')
+# plt.plot(T,S_NO_SOC,"--",color="brown",label='Entropy S')
+# plt.xlabel('Temperature (K)')
+# plt.ylabel('Internal Energy (eV)')
+# plt.title('Energies for Isolated Ce$^{3+}$ vs Temperature')
+# plt.legend()
+# #Save plot to "homework-4-2"
+# directory = 'homework-4-2'
+# if not os.path.exists(directory):            #Create Directory
+#     os.makedirs(directory)
+# plot_filename = os.path.join(directory, "E vs T (isolated Ce3+).png")  #Title of file
+# plt.savefig(plot_filename)
+# #indicate succesful operation 
+# print("Isolated Ce3+ Plot saved to Directory:", directory)
 
 #----------SOC CE3+-------------------------------------------------------------------------------------------------------------------#
 
@@ -104,23 +104,23 @@ U_SOC = -np.gradient(np.log(Z_Ce3plus_SOC), 1 / (k_B * T))   #internal energy
 F_SOC=-k_B*T*np.log(Z_Ce3plus_SOC)         #free-energy
 S_SOC= -np.gradient(F_SOC,T)               #entropy
 
-#Plot 
-plt.figure(figsize=(6, 4))
-plt.plot(T, U_SOC, color='green',label='Internal Energy U')
-plt.plot(T,F_SOC,color="blue",label='Free Energy F')
-plt.plot(T,S_SOC,"--",color="brown",label='Entropy S')
-plt.xlabel('Temperature (K)')
-plt.ylabel('Internal Energy (eV)')
-plt.title('Energy Comparison for Ce$^{3+}$')
-plt.legend()
-#Save plot to "homework-4-2"
-directory = 'homework-4-2'
-if not os.path.exists(directory):            #Create Directory 
-    os.makedirs(directory)
-plot_filename = os.path.join(directory, "E vs T (SOC Ce3+).png")  #Title of file
-plt.savefig(plot_filename)
-#indicate succesful operation 
-print("SOC Ce3+ Plot saved to Directory:", directory)
+# #Plot 
+# plt.figure(figsize=(6, 4))
+# plt.plot(T, U_SOC, color='green',label='Internal Energy U')
+# plt.plot(T,F_SOC,color="blue",label='Free Energy F')
+# plt.plot(T,S_SOC,"--",color="brown",label='Entropy S')
+# plt.xlabel('Temperature (K)')
+# plt.ylabel('Internal Energy (eV)')
+# plt.title('Energy Comparison for Ce$^{3+}$')
+# plt.legend()
+# #Save plot to "homework-4-2"
+# directory = 'homework-4-2'
+# if not os.path.exists(directory):            #Create Directory 
+#     os.makedirs(directory)
+# plot_filename = os.path.join(directory, "E vs T (SOC Ce3+).png")  #Title of file
+# plt.savefig(plot_filename)
+# #indicate succesful operation 
+# print("SOC Ce3+ Plot saved to Directory:", directory)
 
 #-------------SOC and CFS CE3+------------------------------------------------------------------------------------------------------#
 
@@ -134,23 +134,23 @@ U_SOC_CFS = -np.gradient(np.log(Z_Ce3plus_SOC_CFS), 1 / (k_B * T))  #internal en
 F_SOC_CFS=-k_B*T*np.log(Z_Ce3plus_SOC_CFS)          #free-energy
 S_SOC_CFS= -np.gradient(F_SOC_CFS,T)          #entropy
 
-#Plot
-plt.figure(figsize=(6, 4))
-plt.plot(T, U_SOC_CFS, color='green',label='Internal Energy U')
-plt.plot(T,F_SOC_CFS,color="blue",label='Free Energy F')
-plt.plot(T,S_SOC_CFS,"--",color="brown",label='Entropy S')
-plt.xlabel('Temperature (K)')
-plt.ylabel('Internal Energy (eV)')
-plt.title('Energy Comparison for Ce$^{3+}$')
-plt.legend()
-#Save plot to "homework-4-2"
-directory = 'homework-4-2'
-if not os.path.exists(directory):            #Create Directory 
-    os.makedirs(directory)
-plot_filename = os.path.join(directory, "E vs T (SOC-CFS Ce3+).png")  #Title of file
-plt.savefig(plot_filename)
-#indicate succesful operation 
-print("SOC/CFS Ce3+ Plot saved to Directory:", directory)
+# #Plot
+# plt.figure(figsize=(6, 4))
+# plt.plot(T, U_SOC_CFS, color='green',label='Internal Energy U')
+# plt.plot(T,F_SOC_CFS,color="blue",label='Free Energy F')
+# plt.plot(T,S_SOC_CFS,"--",color="brown",label='Entropy S')
+# plt.xlabel('Temperature (K)')
+# plt.ylabel('Internal Energy (eV)')
+# plt.title('Energy Comparison for Ce$^{3+}$')
+# plt.legend()
+# #Save plot to "homework-4-2"
+# directory = 'homework-4-2'
+# if not os.path.exists(directory):            #Create Directory 
+#     os.makedirs(directory)
+# plot_filename = os.path.join(directory, "E vs T (SOC-CFS Ce3+).png")  #Title of file
+# plt.savefig(plot_filename)
+# #indicate succesful operation 
+# print("SOC/CFS Ce3+ Plot saved to Directory:", directory)
 
 #---------------Create Excel file----------------------------------------------------------------------------------------------#
 
@@ -171,3 +171,56 @@ df = pd.DataFrame({
 #Save data to excel file
 df.to_csv('homework-4-2/Energies_vs_Temperature.csv', index=False)
 print("CSV file 'Energies_vs_Temperature.csv' created successfully.")
+
+
+plt.figure(figsize=(6, 4))
+plt.plot(T, U_SOC_CFS, color='green',label='U-SOC&CFS')
+plt.plot(T,U_NO_SOC,color="blue",label='U-Isolated')
+plt.plot(T,U_SOC,"--",color="brown",label='U-SOC')
+plt.xlabel('Temperature (K)')
+plt.ylabel('Internal Energy (eV)')
+plt.title('Internal Energy Comparison for Ce$^{3+}$')
+plt.legend()
+#Save plot to "homework-4-2"
+directory = 'homework-4-2'
+if not os.path.exists(directory):            #Create Directory
+    os.makedirs(directory)
+plot_filename = os.path.join(directory, "Internal Energy vs T.png")  #Title of file
+plt.savefig(plot_filename)
+#indicate succesful operation 
+print("Internal Energy comparison Plot saved to Directory:", directory)
+
+plt.figure(figsize=(6, 4))
+plt.plot(T, F_SOC_CFS, color='green',label='F-SOC&CFS')
+plt.plot(T,F_NO_SOC,color="blue",label='F-Isolated')
+plt.plot(T,F_SOC,"--",color="brown",label='F-SOC')
+plt.xlabel('Temperature (K)')
+plt.ylabel('Internal Energy (eV)')
+plt.title('Free Energy Comparison for Ce$^{3+}$')
+plt.legend()
+#Save plot to "homework-4-2"
+directory = 'homework-4-2'
+if not os.path.exists(directory):            #Create Directory
+    os.makedirs(directory)
+plot_filename = os.path.join(directory, "Free Energy vs T.png")  #Title of file
+plt.savefig(plot_filename)
+#indicate succesful operation 
+print("Free Energy comparison Plot saved to Directory:", directory)
+
+
+plt.figure(figsize=(6, 4))
+plt.plot(T, S_SOC_CFS, color='green',label='S-SOC&CFS')
+plt.plot(T,S_NO_SOC,color="blue",label='S-Isolated')
+plt.plot(T,S_SOC,"--",color="brown",label='S-SOC')
+plt.xlabel('Temperature (K)')
+plt.ylabel('Internal Energy (eV)')
+plt.title('Entropy Comparison for Ce$^{3+}$')
+plt.legend()
+#Save plot to "homework-4-2"
+directory = 'homework-4-2'
+if not os.path.exists(directory):            #Create Directory
+    os.makedirs(directory)
+plot_filename = os.path.join(directory, "Entropy vs T.png")  #Title of file
+plt.savefig(plot_filename)
+#indicate succesful operation 
+print("Entropy  comparison Plot saved to Directory:", directory)
